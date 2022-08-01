4e23de0ccdd06f05a7a7b9586196a64f4b7a322a
Sayantan Karmakar <sayantankarmakar@outlook.com>
2022-08-01 16:28:50 +0700

    added deposit srm/msrm

--- programs/serum-gov/src/lib.rs
+++ programs/serum-gov/src/lib.rs
@@ -1,5 +1,5 @@
 use anchor_lang::prelude::*;
-use anchor_spl::token::{Mint, Token, TokenAccount};
+use anchor_spl::token::{Mint, Token, TokenAccount, Transfer};
 
 declare_id!("G8aGmsybmGqQisBuRrDqiGfdz8YcCJcU3agWDFmTkf8S");
 
@@ -9,11 +9,216 @@ pub use utils::mints::{MSRM, SRM};
 
 #[program]
 pub mod serum_gov {
+    use anchor_spl::token;
+
     use super::*;
 
     pub fn init_vaults(_ctx: Context<InitVaults>) -> Result<()> {
         Ok(())
     }
+
+    pub fn init_user(ctx: Context<InitUser>) -> Result<()> {
+        let user = &mut ctx.accounts.user_account;
+
+        user.owner = ctx.accounts.owner.key();
+        user.bump = *ctx.bumps.get("user_account").unwrap();
+        user.locker_index = 0;
+
+        Ok(())
+    }
+
+    pub fn deposit_srm(
+        ctx: Context<DepositSRM>,
+        amount: u64,
+        collect_delay: i64,
+        redeem_delay: i64,
+    ) -> Result<()> {
+        token::transfer(ctx.accounts.into_deposit_srm_context(), amount)?;
+
+        let locker = &mut ctx.accounts.locker;
+        locker.owner = ctx.accounts.owner.key();
+        locker.is_msrm = false;
+        locker.bump = *ctx.bumps.get("locker").unwrap();
+        locker.created_at = ctx.accounts.clock.unix_timestamp;
+        locker.amount = amount;
+        locker.collect_delay = collect_delay;
+        locker.redeem_delay = redeem_delay;
+
+        let user_account = &mut ctx.accounts.user_account;
+        user_account.locker_index += 1;
+
+        Ok(())
+    }
+
+    pub fn deposit_msrm(
+        ctx: Context<DepositMSRM>,
+        amount: u64,
+        collect_delay: i64,
+        redeem_delay: i64,
+    ) -> Result<()> {
+        token::transfer(ctx.accounts.into_deposit_msrm_context(), amount)?;
+
+        let locker = &mut ctx.accounts.locker;
+        locker.owner = ctx.accounts.owner.key();
+        locker.is_msrm = true;
+        locker.bump = *ctx.bumps.get("locker").unwrap();
+        locker.created_at = ctx.accounts.clock.unix_timestamp;
+        locker.amount = amount;
+        locker.collect_delay = collect_delay;
+        locker.redeem_delay = redeem_delay;
+
+        let user_account = &mut ctx.accounts.user_account;
+        user_account.locker_index += 1;
+
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct DepositMSRM<'info> {
+    #[account(mut)]
+    pub owner: Signer<'info>,
+
+    #[account(
+        mut,
+        seeds = [b"user", &owner.key().to_bytes()[..]],
+        bump,
+    )]
+    pub user_account: Account<'info, User>,
+
+    #[cfg_attr(
+        not(feature = "test"),
+        account(address = MSRM),
+    )]
+    pub msrm_mint: Account<'info, Mint>,
+
+    #[account(
+        mut,
+        associated_token::mint = msrm_mint,
+        associated_token::authority = owner
+    )]
+    pub owner_msrm_account: Account<'info, TokenAccount>,
+
+    /// CHECK: Just a PDA for vault authorities.
+    #[account(
+        seeds = [b"authority"],
+        bump,
+    )]
+    pub vault_authority: AccountInfo<'info>,
+
+    #[account(
+        mut,
+        token::mint = msrm_mint,
+        token::authority = vault_authority,
+    )]
+    pub msrm_vault: Account<'info, TokenAccount>,
+
+    #[account(
+        init,
+        payer = owner,
+        seeds = [b"locker", &owner.key().to_bytes()[..], user_account.locker_index.to_string().as_bytes()],
+        bump,
+        space =  8 + std::mem::size_of::<Locker>()
+    )]
+    pub locker: Account<'info, Locker>,
+
+    pub clock: Sysvar<'info, Clock>,
+    pub token_program: Program<'info, Token>,
+    pub system_program: Program<'info, System>,
+}
+
+impl<'info> DepositMSRM<'info> {
+    fn into_deposit_msrm_context(&self) -> CpiContext<'_, '_, '_, 'info, Transfer<'info>> {
+        let cpi_accounts = Transfer {
+            from: self.owner_msrm_account.to_account_info().clone(),
+            to: self.msrm_vault.to_account_info().clone(),
+            authority: self.owner.to_account_info().clone(),
+        };
+        let cpi_program = self.token_program.to_account_info();
+        CpiContext::new(cpi_program, cpi_accounts)
+    }
+}
+
+#[derive(Accounts)]
+pub struct DepositSRM<'info> {
+    #[account(mut)]
+    pub owner: Signer<'info>,
+
+    #[account(
+        mut,
+        seeds = [b"user", &owner.key().to_bytes()[..]],
+        bump,
+    )]
+    pub user_account: Account<'info, User>,
+
+    #[cfg_attr(
+        not(feature = "test"),
+        account(address = SRM),
+    )]
+    pub srm_mint: Account<'info, Mint>,
+
+    #[account(
+        mut,
+        associated_token::mint = srm_mint,
+        associated_token::authority = owner
+    )]
+    pub owner_srm_account: Account<'info, TokenAccount>,
+
+    /// CHECK: Just a PDA for vault authorities.
+    #[account(
+        seeds = [b"authority"],
+        bump,
+    )]
+    pub vault_authority: AccountInfo<'info>,
+
+    #[account(
+        mut,
+        token::mint = srm_mint,
+        token::authority = vault_authority,
+    )]
+    pub srm_vault: Account<'info, TokenAccount>,
+
+    #[account(
+        init,
+        payer = owner,
+        seeds = [b"locker", &owner.key().to_bytes()[..], user_account.locker_index.to_string().as_bytes()],
+        bump,
+        space =  8 + std::mem::size_of::<Locker>()
+    )]
+    pub locker: Account<'info, Locker>,
+
+    pub clock: Sysvar<'info, Clock>,
+    pub token_program: Program<'info, Token>,
+    pub system_program: Program<'info, System>,
+}
+
+impl<'info> DepositSRM<'info> {
+    fn into_deposit_srm_context(&self) -> CpiContext<'_, '_, '_, 'info, Transfer<'info>> {
+        let cpi_accounts = Transfer {
+            from: self.owner_srm_account.to_account_info().clone(),
+            to: self.srm_vault.to_account_info().clone(),
+            authority: self.owner.to_account_info().clone(),
+        };
+        let cpi_program = self.token_program.to_account_info();
+        CpiContext::new(cpi_program, cpi_accounts)
+    }
+}
+
+#[derive(Accounts)]
+pub struct InitUser<'info> {
+    #[account(mut)]
+    pub owner: Signer<'info>,
+
+    #[account(
+        init,
+        payer = owner,
+        seeds = [b"user", &owner.key().to_bytes()[..]],
+        bump,
+        space = 8 + std::mem::size_of::<User>()
+    )]
+    pub user_account: Account<'info, User>,
+
+    pub system_program: Program<'info, System>,
 }
 
 #[derive(Accounts)]
@@ -62,6 +267,25 @@ pub struct InitVaults<'info> {
     pub msrm_vault: Account<'info, TokenAccount>,
 
     pub rent: Sysvar<'info, Rent>,
+    // pub associated_token_program: Program<'info, AssociatedToken>,
     pub token_program: Program<'info, Token>,
     pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct User {
+    pub owner: Pubkey,
+    pub bump: u8,
+    pub locker_index: u64,
+}
+
+#[account]
+pub struct Locker {
+    pub owner: Pubkey,
+    pub is_msrm: bool,
+    pub bump: u8,
+    pub created_at: i64,
+    pub amount: u64,
+    pub collect_delay: i64,
+    pub redeem_delay: i64,
 }
--- tests/serum-gov.ts
+++ tests/serum-gov.ts
@@ -5,20 +5,19 @@ import {
   LAMPORTS_PER_SOL,
   PublicKey,
   SystemProgram,
+  SYSVAR_CLOCK_PUBKEY,
   SYSVAR_RENT_PUBKEY,
 } from "@solana/web3.js";
 import {
-  Account,
   createAssociatedTokenAccount,
   createMint,
-  getAssociatedTokenAddress,
-  getOrCreateAssociatedTokenAccount,
   mintTo,
   TOKEN_PROGRAM_ID,
 } from "@solana/spl-token";
 import { SerumGov } from "../target/types/serum_gov";
 import { findProgramAddressSync } from "@project-serum/anchor/dist/cjs/utils/pubkey";
-import { assert } from "chai";
+import { assert, expect } from "chai";
+import { BN } from "bn.js";
 
 describe("serum-gov", () => {
   // Configure the client to use the local cluster.
@@ -41,19 +40,24 @@ describe("serum-gov", () => {
   let srmVault: PublicKey;
   let msrmVault: PublicKey;
 
+  let aliceUserAccount: PublicKey;
+
   before(async () => {
+    // Airdrop sbf
     const sbfDrop = await connection.requestAirdrop(
       sbf.publicKey,
       5 * LAMPORTS_PER_SOL
     );
     await connection.confirmTransaction(sbfDrop);
 
+    // Airdrop alice
     const aliceDrop = await connection.requestAirdrop(
       alice.publicKey,
       5 * LAMPORTS_PER_SOL
     );
     await connection.confirmTransaction(aliceDrop);
 
+    // Create SRM mint
     SRM_MINT = await createMint(
       connection,
       sbf,
@@ -65,6 +69,8 @@ describe("serum-gov", () => {
         commitment: "confirmed",
       }
     );
+
+    // Create MSRM mint
     MSRM_MINT = await createMint(
       connection,
       sbf,
@@ -77,12 +83,15 @@ describe("serum-gov", () => {
       }
     );
 
+    // Create alice SRM account
     aliceSRMAccount = await createAssociatedTokenAccount(
       connection,
       alice,
       SRM_MINT,
       alice.publicKey
     );
+
+    // Create alice MSRM account
     aliceMSRMAccount = await createAssociatedTokenAccount(
       connection,
       alice,
@@ -90,6 +99,7 @@ describe("serum-gov", () => {
       alice.publicKey
     );
 
+    // Mint SRM to alice
     await mintTo(
       connection,
       sbf,
@@ -99,6 +109,7 @@ describe("serum-gov", () => {
       BigInt(200 * 1000000)
     );
 
+    // Mint MSRM to alice
     await mintTo(connection, sbf, MSRM_MINT, aliceMSRMAccount, sbf, 2);
 
     [vaultAuthority] = findProgramAddressSync(
@@ -111,6 +122,10 @@ describe("serum-gov", () => {
     );
     [msrmVault] = findProgramAddressSync(
       [Buffer.from("vault"), MSRM_MINT.toBuffer()],
+      program.programId
+    );
+    [aliceUserAccount] = findProgramAddressSync(
+      [Buffer.from("user"), alice.publicKey.toBuffer()],
       program.programId
     );
   });
@@ -155,5 +170,104 @@ describe("serum-gov", () => {
     } catch (e) {
       assert(true);
     }
+  });
+
+  it("can init user", async () => {
+    const tx = await program.methods
+      .initUser()
+      .accounts({
+        owner: alice.publicKey,
+        userAccount: aliceUserAccount,
+        systemProgram: SystemProgram.programId,
+      })
+      .signers([alice])
+      .rpc();
+
+    const aliceAccount = await program.account.user.fetch(aliceUserAccount);
+
+    expect(aliceAccount.owner.toBase58()).to.equal(alice.publicKey.toBase58());
+    expect(aliceAccount.lockerIndex.toNumber()).to.equal(0);
+  });
+
+  it("can deposit srm", async () => {
+    const aliceAccount = await program.account.user.fetch(aliceUserAccount);
+    const [locker] = findProgramAddressSync(
+      [
+        Buffer.from("locker"),
+        alice.publicKey.toBuffer(),
+        Buffer.from(aliceAccount.lockerIndex.toString()),
+      ],
+      program.programId
+    );
+
+    await program.methods
+      .depositSrm(new BN(100_000_000), new BN(0), new BN(1_000))
+      .accounts({
+        owner: alice.publicKey,
+        userAccount: aliceUserAccount,
+        srmMint: SRM_MINT,
+        ownerSrmAccount: aliceSRMAccount,
+        vaultAuthority,
+        srmVault,
+        locker,
+        clock: SYSVAR_CLOCK_PUBKEY,
+        tokenProgram: TOKEN_PROGRAM_ID,
+        systemProgram: SystemProgram.programId,
+      })
+      .signers([alice])
+      .rpc();
+
+    const aliceLocker = await program.account.locker.fetch(locker);
+    const srmVaultBalance = await connection.getTokenAccountBalance(srmVault);
+
+    expect(aliceLocker.owner.toBase58()).to.equal(alice.publicKey.toBase58());
+    expect(aliceLocker.amount.toNumber()).to.equal(100_000_000);
+    expect(aliceLocker.isMsrm).to.equal(false);
+    expect(aliceLocker.collectDelay.toNumber()).to.equal(0);
+    expect(aliceLocker.redeemDelay.toNumber()).to.equal(1_000);
+
+    expect(srmVaultBalance.value.uiAmount).to.equal(100);
+  });
+
+  it("can deposit msrm", async () => {
+    const aliceAccount = await program.account.user.fetch(aliceUserAccount);
+    const [msrmLocker] = findProgramAddressSync(
+      [
+        Buffer.from("locker"),
+        alice.publicKey.toBuffer(),
+        Buffer.from(aliceAccount.lockerIndex.toString()),
+      ],
+      program.programId
+    );
+
+    await program.methods
+      .depositMsrm(new BN(1), new BN(0), new BN(1_000))
+      .accounts({
+        owner: alice.publicKey,
+        userAccount: aliceUserAccount,
+        msrmMint: MSRM_MINT,
+        ownerMsrmAccount: aliceMSRMAccount,
+        vaultAuthority,
+        msrmVault,
+        locker: msrmLocker,
+        clock: SYSVAR_CLOCK_PUBKEY,
+        tokenProgram: TOKEN_PROGRAM_ID,
+        systemProgram: SystemProgram.programId,
+      })
+      .signers([alice])
+      .rpc({ skipPreflight: true });
+
+    const aliceMSRMLocker = await program.account.locker.fetch(msrmLocker);
+    const msrmVaultBalance = await connection.getTokenAccountBalance(msrmVault);
+
+    expect(aliceMSRMLocker.owner.toBase58()).to.equal(
+      alice.publicKey.toBase58()
+    );
+    expect(aliceMSRMLocker.amount.toNumber()).to.equal(1);
+    expect(aliceMSRMLocker.isMsrm).to.equal(true);
+    expect(aliceMSRMLocker.collectDelay.toNumber()).to.equal(0);
+    expect(aliceMSRMLocker.redeemDelay.toNumber()).to.equal(1_000);
+
+    expect(msrmVaultBalance.value.uiAmount).to.equal(1);
   });
 });
